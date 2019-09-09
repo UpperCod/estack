37da81845cf77d2c58cbd4fe8a879d2d7b978ce3
UpperCod <mtrujillo@outlook.cl>
2019-09-09 02:04:44 -0300

    update dir

--- example/dist/index.js
+++ example/dist/index.js
@@ -1,24 +1,2 @@
-var style = `:root {
-	--color: black;
-}
-button {
-	color: orange;
-	size: 20px;
-}
-megalodonte {
-	color: orange;
-}
-.box {
-	width: 100px;
-	height: 100px;
-	color: black;
-}
-`;
-
-var div = document.createElement("div");
-document.appendChild(div);
-div.innerHTML = "<h1>App 1</h1>";
-div.addEventListener("click", function () {
-  console.log(style);
-});
+let e=document.createElement("div");document.appendChild(e),e.innerHTML="<h1>App 1</h1>",e.addEventListener("click",()=>{console.log("button{color:orange;size:20px}.box{width:100px;height:100px;color:#000}")});
 //# sourceMappingURL=index.js.map
--- example/src/app-2.js
+++
@@ -1,9 +0,0 @@
-import style from "./style/app-1.css";
-
-let div = document.createElement("div");
-document.appendChild(div);
-div.innerHTML = "<h1>App 1</h1>";
-
-div.addEventListener("click", () => {
-	console.log(style);
-});
