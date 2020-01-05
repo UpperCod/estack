d1ce8bb6cc55784e6021f446d9c44b371a6439ad
UpperCod <mtrujillo@outlook.cl>
2020-01-05 12:39:06 -0300

    copy

--- example/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { h } from 'https://unpkg.com/atomico@0.17.0/core.js?module';
-
-console.log({
-  node: document.querySelector("*"),
-  d: h,
-  data: import('//localhost:8080/header.js')
-});
-//# sourceMappingURL=index.js.map
--- example/dist/typescript.js
+++
@@ -1,6 +0,0 @@
-function typescript (props) {
-  return h("div", null, "hola ", props.age);
-}
-
-export default typescript;
-//# sourceMappingURL=typescript.js.map
