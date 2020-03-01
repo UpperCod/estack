5911f2aed9b30a8c6e286793ed260a76fa5c8715
UpperCod <mtrujillo@outlook.cl>
2020-03-01 14:59:05 -0300

    export is improved from html

--- src/read-html.js
+++ src/read-html.js
@@ -5,9 +5,11 @@ export async function readHtml({ code, addFile }) {
   const meta = {};
   const astHtml = await analyzeHtml(code, async node => {
     if (node.nodeName == "link") {
-      const isCss = node.getAttribute("rel") == "stylesheet";
+      const isHrefImport = ["stylesheet", "manifest", "preload"].includes(
+        node.getAttribute("rel")
+      );
       const href = node.getAttribute("href");
-      if (isCss && href && !isUrl(href)) {
+      if (isHrefImport && href && !isUrl(href)) {
         node.setAttribute("href", await addFile(href));
       }
     } else if (["script", "img", "video"].includes(node.nodeName)) {
