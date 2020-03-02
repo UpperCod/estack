0af48f6e2aadc1fb09456d353ec36926cd0aeacb
UpperCod <mtrujillo@outlook.cl>
2020-03-02 00:27:45 -0300

    add support to parseFragment

--- src/read-html.js
+++ src/read-html.js
@@ -1,26 +1,26 @@
 import { analyzeHtml, serializeHtml } from "./analize-html";
 import { isUrl } from "./utils";
 
-export async function readHtml({ code, addFile }) {
-  const meta = {};
-  const astHtml = await analyzeHtml(code, async node => {
-    if (node.nodeName == "link") {
-      const isHrefImport = ["stylesheet", "manifest", "preload"].includes(
-        node.getAttribute("rel")
-      );
-      const href = node.getAttribute("href");
-      if (isHrefImport && href && !isUrl(href)) {
-        node.setAttribute("href", await addFile(href));
+export async function readHtml({ code, addFile, useFragment }) {
+  const astHtml = await analyzeHtml(
+    code,
+    async node => {
+      if (node.nodeName == "link") {
+        const isHrefImport = ["stylesheet", "manifest", "preload"].includes(
+          node.getAttribute("rel")
+        );
+        const href = node.getAttribute("href");
+        if (isHrefImport && href && !isUrl(href)) {
+          node.setAttribute("href", await addFile(href));
+        }
+      } else if (["script", "img", "video"].includes(node.nodeName)) {
+        const src = node.getAttribute("src");
+        if (src && !isUrl(src)) {
+          node.setAttribute("src", await addFile(src));
+        }
       }
-    } else if (["script", "img", "video"].includes(node.nodeName)) {
-      const src = node.getAttribute("src");
-      if (src && !isUrl(src)) {
-        node.setAttribute("src", await addFile(src));
-      }
-    }
-  });
-  return {
-    code: serializeHtml(astHtml),
-    meta
-  };
+    },
+    useFragment
+  );
+  return serializeHtml(astHtml);
 }
