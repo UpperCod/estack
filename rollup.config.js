0a889a4434687223fb91ca0707f11321bcc75989
UpperCod <mtrujillo@outlook.cl>
2020-05-15 00:42:27 -0400

    add chokidar to the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import builtins from "builtin-modules";
 import pkg from "./package.json";
+import conditionalFsEventsImport from "./build-plugins/conditional-fsevents-import/conditional-fsevents-import";
 
 export default {
   input: "src/cli.js",
@@ -19,6 +20,7 @@ export default {
     }),
     resolve({}),
     json(),
+    conditionalFsEventsImport(),
     common(),
   ],
 };
