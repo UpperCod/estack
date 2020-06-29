8549cce7ba2252693a80eefad492559181d18d2b
UpperCod <mtrujillo@outlook.cl>
2020-06-29 16:33:15 -0400

    Add more JSDOC type definition

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,40 @@
+/**
+ * Logger marker for the build process used by the core
+ */
 export const MARK_ROOT = "ESbuild";
+/**
+ * Logger marker for the build process used by Rollup
+ */
 export const MARK_ROLLUP = "Rollup";
+/**
+ * Start message for transformation syntax errors
+ */
 export const ERROR_TRANSFORMING = "SyntaxError: Error transforming";
+/**
+ * Start message for request errors
+ */
 export const ERROR_FETCH = "FetchError:";
+/**
+ * Start message for errors for file not found
+ */
 export const ERROR_FILE_NOT_FOUNT = "FileNotFound:";
+/**
+ * Index to define private values in the context used by liquidjs.
+ * Allows access to the fragments object
+ */
 export const DATA_FRAGMENTS = Symbol("_fragments");
+/**
+ * Index to define private values in the context used by liquidjs.
+ * Allows access to the layout associated with the render instance
+ */
 export const DATA_LAYOUT = Symbol("_layout");
+/**
+ * Index to define private values in the context used by liquidjs.
+ * Allows access to the page associated with the render instance
+ */
 export const DATA_PAGE = Symbol("_page");
+/**
+ * Index to define private values in the context used by liquidjs.
+ * It allows defining if the render instance is executed from layout
+ */
 export const FROM_LAYOUT = Symbol("_fromLayout");
