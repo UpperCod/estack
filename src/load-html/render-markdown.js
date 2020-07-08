dde460bea323c2fd6ac3577c94ab0c5c29e9f808
UpperCod <mtrujillo@outlook.cl>
2020-07-08 00:43:40 -0400

    Language support for markdown expanded

--- src/load-html/render-markdown.js
+++ src/load-html/render-markdown.js
@@ -6,15 +6,21 @@ import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-css";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-markdown";
-import "prismjs/components/prism-twig";
 import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-ruby";
 import "prismjs/components/prism-scss";
 import "prismjs/components/prism-sass";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-liquid";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-graphql";
+import "prismjs/components/prism-http";
+import "prismjs/components/prism-git";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-rust";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-python";
+import "prismjs/components/prism-diff";
 
 let cache = {};
 
