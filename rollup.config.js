baa3a02d01fb7ea21734c5ecf50886404f12dd0b
UpperCod <mtrujillo@outlook.cl>
2020-08-01 11:50:55 -0400

    remove the sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -7,34 +7,33 @@ import pkg from "./package.json";
 import conditionalFsEventsImport from "./build-plugins/conditional-fsevents-import/conditional-fsevents-import";
 
 export default {
-  input: "src/cli.js",
-  output: {
-    file: "cli.js",
-    format: "cjs",
-    banner: "#!/usr/bin/env node",
-    externalLiveBindings: false,
-    freeze: false,
-    interop: false,
-    sourcemap: true,
-  },
-  external: [...builtins, ...Object.keys(pkg.dependencies)],
-  //treeshake: {
-  //  moduleSideEffects: false,
-  //  propertyReadSideEffects: false,
-  //  tryCatchDeoptimization: false,
-  //},
-  plugins: [
-    replace({
-      "PKG.VERSION": pkg.version,
-    }),
-    ...(process.env.ROLLUP_WATCH
-      ? []
-      : [
-          resolve({}),
-          json(),
-          conditionalFsEventsImport(),
-          common(),
-          // pluginTerser({ sourcemap: true }),
-        ]),
-  ],
+    input: "src/cli.js",
+    output: {
+        file: "cli.js",
+        format: "cjs",
+        banner: "#!/usr/bin/env node",
+        externalLiveBindings: false,
+        freeze: false,
+        interop: false,
+    },
+    external: [...builtins, ...Object.keys(pkg.dependencies)],
+    //treeshake: {
+    //  moduleSideEffects: false,
+    //  propertyReadSideEffects: false,
+    //  tryCatchDeoptimization: false,
+    //},
+    plugins: [
+        replace({
+            "PKG.VERSION": pkg.version,
+        }),
+        ...(process.env.ROLLUP_WATCH
+            ? []
+            : [
+                  resolve({}),
+                  json(),
+                  conditionalFsEventsImport(),
+                  common(),
+                  // pluginTerser({ sourcemap: true }),
+              ]),
+    ],
 };
