1e9763afbf7c0fb31d6468d70c30a4a55e8c525e
UpperCod <mtrujillo@outlook.cl>
2020-09-16 12:45:02 -0300

    fix configuration for test

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,11 +1,14 @@
+import path from "path";
+import { readFile } from "fs/promises";
 import { build } from "../src/build";
 import { expect } from "chai";
 
 const src = "./tests/basic/index.html";
+const org = "./tests/basic/src";
 const dest = "./tests/basic/public";
 
-describe("Status and content", () => {
-    it("a", async () => {
+describe("Build", () => {
+    it("check read and write", async () => {
         const { files } = await build({
             mode: "build",
             src,
@@ -13,6 +16,21 @@ describe("Status and content", () => {
             port: 8000,
         });
 
-        expect(10).to.equal(10);
+        const filesSrc = ["code-css.css", "code-js.js"].map((fileName) =>
+            path.join(org, fileName)
+        );
+
+        const filesExpect = [...filesSrc, src].map(path.normalize);
+        // check read
+        filesExpect.map((src) => {
+            expect(files[src]).is.not.null;
+        });
+        // check content
+        Promise.all(
+            filesSrc.map(async (src) => {
+                const content = await readFile(src, "utf8");
+                expect(files[src].content.trim()).to.equal(content.trim());
+            })
+        );
     });
 });
--- tests/utils.js
+++
@@ -1,6 +0,0 @@
-const normalizeData = (str) => {
-  str = Array.isArray(str) ? str.join("\n") : str;
-  return str.replace(/(\r\n|\n|\r)/gm, "");
-};
-
-module.exports = { normalizeData };
