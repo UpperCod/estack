69c606163e5236d25d3fa318f1f0cb4db3e9543e
UpperCod <mtrujillo@outlook.cl>
2020-09-22 16:06:07 -0300

    add internal test

---
+++ src/build/tests/create-build.test.ts
@@ -0,0 +1,95 @@
+import { File } from "estack";
+import { expect } from "chai";
+import { createBuild } from "../create-build";
+
+const build = createBuild(
+    {
+        // actions must dispatch in order, first watch and then load
+        async load(file) {},
+        watch(file) {},
+    },
+    {
+        types: {
+            ts: "js",
+        },
+        assets: "",
+        href: "",
+    }
+);
+
+describe("createBuild", function () {
+    /**
+     * activation of actions
+     */
+    it("activation of actions when adding file", function (done) {
+        let prevent = true;
+        const task: { index: string; value: File }[] = [];
+        const addTask = (index: string, value: File) => {
+            task.push({ index, value });
+            if (task.length == 2 && prevent) {
+                prevent = false;
+                // the time out is to check if the actions have been duplicated
+                setTimeout(() => {
+                    expect(task.length).to.equal(2);
+                    expect(task[0]).to.deep.equal({
+                        index: "watch",
+                        value: file,
+                    });
+                    expect(task[1]).to.deep.equal({
+                        index: "load",
+                        value: file,
+                    });
+                    done();
+                }, 10);
+            }
+        };
+        const build = createBuild(
+            {
+                // actions must dispatch in order, first watch and then load
+                async load(file) {
+                    addTask("load", file);
+                },
+                watch(file) {
+                    addTask("watch", file);
+                },
+            },
+            {
+                types: {
+                    ts: "js",
+                },
+                assets: "",
+                href: "",
+            }
+        );
+        const src = "file.ts";
+        const file = build.addFile(src);
+        file.load();
+    });
+    /**
+     * file properties
+     */
+    it("createBuild and addFile", function () {
+        const src = "file.ts";
+        const file = build.addFile(src);
+        expect(build.getFile(src)).to.equal(file);
+        // the following settings are minimal
+        // for file processing operation
+        expect(file.type).to.equal("js");
+        expect(file.watch).is.true;
+        expect(file.hash).is.false;
+        expect(file.root).is.false;
+        expect(file.asset).is.false;
+        expect(file.src).is.string;
+        expect(file.importers).to.deep.equal({});
+        expect(file.errors).to.deep.equal([]);
+    });
+    it("addImporter", function () {
+        const file1 = build.addFile("file1.js");
+        const file2 = build.addFile("file2.js");
+        build.addImporter(file1, file2);
+
+        expect(file1.importers).to.deep.include({
+            [file2.src]: { rewrite: true },
+        });
+    });
+});
